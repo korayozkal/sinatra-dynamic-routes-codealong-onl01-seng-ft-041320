4762c5c26227eabe51b33ec03c06d7c0931a0a6e
Koray Ozkal <koryozkal@gmail.com>
2020-06-08 04:37:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   # This is a sample static route.
-  get '/hello' do
-    "Hello World!"
-  end
-
-  # This is a sample dynamic route.
-  get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
+ 
 get "/goodbye/:name" do
     status 200
     @user_name = params[:name]
