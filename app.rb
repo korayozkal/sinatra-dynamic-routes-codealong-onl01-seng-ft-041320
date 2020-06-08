c99d99214dc89b6e7f721690c67d9413178290eb
Koray Ozkal <koryozkal@gmail.com>
2020-06-08 04:44:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,8 +19,7 @@ get "/goodbye/:name" do
 		"Goodbye #{params[:name]}!"
 	end
 
-
-end 		get "/multiply/:num1/:num2" do
+get "/multiply/:num1/:num2" do
 		"#{params[:num1].to_i*params[:num2].to_i}"
 
 end
